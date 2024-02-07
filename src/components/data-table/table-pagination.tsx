364a6f42571ecc9590be22703d98dcc8efd795c5
ION <thegoshagames@gmail.com>
2024-02-07 10:21:16 +0300

    feature/74

--- src/components/data-table/table-pagination.tsx
+++ src/components/data-table/table-pagination.tsx
@@ -3,48 +3,42 @@ import {
     ChevronRightIcon,
     DoubleArrowLeftIcon,
     DoubleArrowRightIcon,
-} from '@radix-ui/react-icons'
-import { Table } from '@tanstack/react-table'
+} from "@radix-ui/react-icons";
+import { Table } from "@tanstack/react-table";
 
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 import {
     Select,
     SelectContent,
     SelectItem,
     SelectTrigger,
     SelectValue,
-} from '@/components/ui/select'
-
-const ITEMS_PER_PAGE_LIST = [10, 20, 30, 40, 50]
+} from "@/components/ui/select";
 
 interface TablePaginationProps<TData> {
-    table?: Table<TData>
+    table: Table<TData>;
 }
 
 export function TablePagination<TData>({ table }: TablePaginationProps<TData>) {
-    const totalPagesCount =
-        typeof table?.getPageCount() !== 'undefined' &&
-        Array.from({ length: table?.getPageCount() }, (_, i) => i)
-
     return (
         <div className="w-full flex items-center justify-between px-2 mt-6">
             <div className="w-full flex items-center justify-between space-x-6 lg:space-x-8">
                 <div className="flex items-center space-x-2">
                     <Select
-                        value={`${table?.getState().pagination.pageSize}`}
+                        value={`${table.getState().pagination.pageSize}`}
                         onValueChange={(value) => {
-                            table?.setPageSize(Number(value))
+                            table.setPageSize(Number(value));
                         }}
                     >
                         <SelectTrigger className="h-8 w-[70px]">
                             <SelectValue
                                 placeholder={
-                                    table?.getState().pagination.pageSize
+                                    table.getState().pagination.pageSize
                                 }
                             />
                         </SelectTrigger>
                         <SelectContent side="top">
-                            {ITEMS_PER_PAGE_LIST.map((pageSize) => (
+                            {[10, 20, 30, 40, 50].map((pageSize) => (
                                 <SelectItem
                                     key={pageSize}
                                     value={`${pageSize}`}
@@ -59,8 +53,8 @@ export function TablePagination<TData>({ table }: TablePaginationProps<TData>) {
                     <Button
                         variant="outline"
                         className="hidden h-8 w-8 p-0 lg:flex bg-[#E7F3FA] text-primary"
-                        onClick={() => table?.setPageIndex(0)}
-                        disabled={!table?.getCanPreviousPage()}
+                        onClick={() => table.setPageIndex(0)}
+                        disabled={!table.getCanPreviousPage()}
                     >
                         <span className="sr-only">На первую страницу</span>
                         <DoubleArrowLeftIcon className="h-4 w-4" />
@@ -68,36 +62,17 @@ export function TablePagination<TData>({ table }: TablePaginationProps<TData>) {
                     <Button
                         variant="outline"
                         className="h-8 w-8 p-0 bg-[#E7F3FA] text-primary"
-                        onClick={() => table?.previousPage()}
-                        disabled={!table?.getCanPreviousPage()}
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
                     >
                         <span className="sr-only">Предыдущая страница</span>
                         <ChevronLeftIcon className="h-4 w-4" />
                     </Button>
-                    {totalPagesCount &&
-                        totalPagesCount.map((page) => {
-                            const isCurrentPage =
-                                table.getState().pagination.pageIndex === page
-                            return (
-                                <Button
-                                    key={page}
-                                    variant="ghost"
-                                    className={`h-8 w-8 p-0 font-normal ${
-                                        isCurrentPage
-                                            ? 'bg-primary text-white'
-                                            : ''
-                                    }`}
-                                    onClick={() => table?.setPageIndex(page)}
-                                >
-                                    {page + 1}
-                                </Button>
-                            )
-                        })}
                     <Button
                         variant="outline"
                         className="h-8 w-8 p-0 bg-[#E7F3FA] text-primary"
-                        onClick={() => table?.nextPage()}
-                        disabled={!table?.getCanNextPage()}
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
                     >
                         <span className="sr-only">Следующая страница</span>
                         <ChevronRightIcon className="h-4 w-4" />
@@ -106,9 +81,9 @@ export function TablePagination<TData>({ table }: TablePaginationProps<TData>) {
                         variant="outline"
                         className="hidden h-8 w-8 p-0 lg:flex bg-[#E7F3FA] text-primary"
                         onClick={() =>
-                            table?.setPageIndex(table?.getPageCount() - 1)
+                            table.setPageIndex(table.getPageCount() - 1)
                         }
-                        disabled={!table?.getCanNextPage()}
+                        disabled={!table.getCanNextPage()}
                     >
                         <span className="sr-only">На последнюю страницу</span>
                         <DoubleArrowRightIcon className="h-4 w-4" />
@@ -116,5 +91,5 @@ export function TablePagination<TData>({ table }: TablePaginationProps<TData>) {
                 </div>
             </div>
         </div>
-    )
+    );
 }
