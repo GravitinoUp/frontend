44bbea5bde3640affb40a7557c5c6d903d9fa348
ION (Георгий) <60499486+iongames@users.noreply.github.com>
2024-02-07 18:04:35 +0300

    #74 Добавлены фильтры на странице с Задачами (#1)
    * feature/74

--- src/components/search-input/index.tsx
+++ src/components/search-input/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Search } from 'lucide-react'
+import { Button } from '../ui/button'
+import FilterIcon from '@/assets/icons/filter_icon.svg'
 
 type Props = {
     value: string | number
     onChange: (value: string | number) => void
     debounce?: number
+    suffixIconClick?: () => void
 } & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>
 
 export const DebouncedInput: React.FC<Props> = ({
     value: initialValue,
     onChange,
     debounce = 500,
+    suffixIconClick,
     ...props
 }) => {
     const [value, setValue] = React.useState<number | string>(initialValue)
@@ -31,19 +35,28 @@ export const DebouncedInput: React.FC<Props> = ({
     }, [value])
 
     return (
-        <div className="w-full h-[37px] border-solid border-[1px] rounded-xl flex items-center justify-start mb-4">
+        <div className="w-full h-[37px] border-solid border-[1px] rounded-xl flex items-center justify-start mb-4 overflow-hidden">
             <div className="ml-3 mr-3">
                 <Search color="#8A9099" size={20} />
             </div>
             <div className="flex-auto ">
                 <input
-                    className="w-full focus:outline-none"
+                    className="w-full focus:outline-none bg-transparent"
                     {...props}
                     value={value}
                     onChange={handleInputChange}
                     placeholder="Искать..."
                 />
             </div>
+            {suffixIconClick && (
+                <Button
+                    variant={'ghost'}
+                    className="px-4 rounded-none"
+                    onClick={suffixIconClick}
+                >
+                    <FilterIcon />
+                </Button>
+            )}
         </div>
     )
 }
