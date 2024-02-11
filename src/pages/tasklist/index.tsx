1aa1d97293353a37f58db3b27fea6a8fb4b15201
ION (Георгий) <60499486+iongames@users.noreply.github.com>
2024-02-11 20:20:29 +0300

    #153 Добавлены кнопки экспорта/импорта (#35)
    * Добавлены окна экспорта и импорта файлов

--- src/pages/tasklist/index.tsx
+++ src/pages/tasklist/index.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import AddTaskForm from './add-task-form'
 import { personalOrdersQuery } from './constants'
+import ExportForm from './export-form'
+import ImportForm from './import-form'
 import TaskListContent from './tasklist-content'
+import CloseRounded from '@/assets/icons/close_rounded.svg'
 import CalendarForm from '@/components/calendar-form/calendar-form'
 import CustomTabs from '@/components/custom-tabs/custom-tabs'
 import ExcelButton from '@/components/excel-button/excel-button'
@@ -30,6 +33,8 @@ const tasksPageTabs = [
 export default function TaskListPage() {
     const { refetch } = useGetPersonalOrdersQuery(personalOrdersQuery)
     const [formOpen, setFormOpen] = useState(false)
+    const [exportFormOpen, setExportFormOpen] = useState(false)
+    const [importFormOpen, setImportFormOpen] = useState(false)
 
     return (
         <PageLayout
@@ -46,8 +51,20 @@ export default function TaskListPage() {
                 <div>
                     <CalendarForm open={false} />
                     <div className="flex gap-3">
-                        <ExcelButton type="export" onClick={() => {}} />
-                        <ExcelButton type="import" onClick={() => {}} />
+                        <FormDialog
+                            open={exportFormOpen}
+                            setOpen={setExportFormOpen}
+                            actionButton={<ExcelButton buttonType="export" />}
+                            addItemForm={<ExportForm />}
+                            closeIcon={<CloseRounded />}
+                        />
+                        <FormDialog
+                            open={importFormOpen}
+                            setOpen={setImportFormOpen}
+                            actionButton={<ExcelButton buttonType="import" />}
+                            addItemForm={<ImportForm />}
+                            closeIcon={<CloseRounded />}
+                        />
                     </div>
                 </div>
             }
