3ca00100eb1fa44636ea34c3980a4a933e29fbf5
ION <thegoshagames@gmail.com>
2024-02-12 20:12:18 +0300

    Изменен интерфейс PropertyInterface

--- src/pages/manage-properties/manage-properties-content.tsx
+++ src/pages/manage-properties/manage-properties-content.tsx
@@ -1,17 +1,23 @@
+import { useState } from 'react'
 import { managePropertiesColumns } from './manage-properties-columns'
+import { placeholderQuery } from '../tasklist/constants'
 import { CustomAlert } from '@/components/custom-alert/custom-alert'
 import DataTable from '@/components/data-table/data-table'
 import { LoadingSpinner } from '@/components/spinner/spinner'
 import { useGetPropertiesQuery } from '@/redux/api/properties'
 import { EntityType } from '@/types/interface/fetch'
+import { PropertyPayloadInterface } from '@/types/interface/properties'
 
 interface ManagePropertiesContentProps {
     entity: EntityType
 }
 
 function ManagePropertiesContent({ entity }: ManagePropertiesContentProps) {
+    const [propertiesQuery, setPropertiesQuery] =
+        useState<PropertyPayloadInterface>(placeholderQuery)
+
     const {
-        data = { count: 0, data: [] },
+        data: properties = { count: 0, data: [] },
         isError,
         isLoading,
     } = useGetPropertiesQuery(entity)
@@ -24,7 +30,22 @@ function ManagePropertiesContent({ entity }: ManagePropertiesContentProps) {
         return <CustomAlert />
     }
 
-    return <DataTable data={data.data} columns={managePropertiesColumns} />
+    return (
+        <DataTable
+            data={properties.data}
+            columns={managePropertiesColumns}
+            getPaginationInfo={(pageSize, pageIndex) => {
+                setPropertiesQuery({
+                    ...propertiesQuery,
+                    offset: { count: pageSize, page: pageIndex + 1 },
+                })
+            }}
+            paginationInfo={{
+                itemCount: properties.count,
+                pageSize: propertiesQuery.offset.count,
+            }}
+        />
+    )
 }
 
 export default ManagePropertiesContent
--- src/types/interface/properties.ts
+++ src/types/interface/properties.ts
@@ -1,4 +1,9 @@
-import { EntityType } from './fetch'
+import { EntityType, IQuery, SortOptionsType } from './fetch'
+
+export interface PropertyPayloadInterface extends IQuery {
+    filter: Partial<PropertyInterface>
+    sorts: PropertySortInterface
+}
 
 export interface PropertyInterface {
     property_name_id: number
@@ -12,3 +17,7 @@ export interface PropertyValueInterface {
     property_name_id: number
     property_value: string
 }
+
+export type PropertySortInterface = Partial<
+    Record<keyof PropertyInterface, SortOptionsType>
+>
