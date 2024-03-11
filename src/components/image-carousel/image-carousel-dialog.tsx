ac1390a026c25c9806f06e20b3cd6341d4ed7084
ION <thegoshagames@gmail.com>
2024-03-11 10:40:57 +0300

    Добавлен поворот изображений, изменено диалоговое окно с изображениями

--- src/components/image-carousel/image-carousel-dialog.tsx
+++ src/components/image-carousel/image-carousel-dialog.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { EmblaCarouselType } from 'embla-carousel'
 import { FileData } from '../file-container/multi-file-input'
-import { Button } from '../ui/button'
 import {
     Carousel,
     CarouselContent,
@@ -10,6 +10,9 @@ import {
 } from '../ui/carousel'
 import { Dialog, DialogContent } from '../ui/dialog'
 import CloseRounded from '@/assets/icons/close_rounded.svg'
+import DownloadRounded from '@/assets/icons/download_rounded.svg'
+import RotateIcon from '@/assets/icons/rotate.svg'
+import { cn } from '@/lib/utils'
 
 interface ImageCarouselDialogProps {
     files: FileData[]
@@ -25,10 +28,20 @@ const ImageCarouselDialog = ({
     setOpen,
 }: ImageCarouselDialogProps) => {
     const [rotation, setRotation] = useState(0)
+    const [carouselApi, setCarouselApi] = useState<
+        EmblaCarouselType | undefined
+    >()
+    const [slidesInView, setSlidesInView] = useState<number[]>([])
 
     useEffect(() => {
         console.log(rotation)
     }, [rotation])
+
+    const handleSlidesInView = (api: EmblaCarouselType) => {
+        setSlidesInView(api.slidesInView())
+    }
+
+    carouselApi?.on('slidesInView', handleSlidesInView)
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -36,19 +49,13 @@ const ImageCarouselDialog = ({
                 className="bg-transparent border-none rounded-none w-full max-w-full h-screen shadow-none"
                 onOpenAutoFocus={(e) => e.preventDefault}
                 header={
-                    <div className="absolute pt-4 pl-4">
-                        <Button
-                            variant="ghost"
-                            onClick={() => setRotation(rotation - 90)}
-                        >
-                            Повернуть на 90 влево
-                        </Button>
-                        <Button
-                            variant="ghost"
-                            onClick={() => setRotation(rotation + 90)}
-                        >
-                            Повернуть на 90 вправо
-                        </Button>
+                    <div className="absolute flex gap-5 right-16 pt-4 pl-4">
+                        <div onClick={() => setRotation(rotation + 90)}>
+                            <RotateIcon />
+                        </div>
+                        <div onClick={() => {}}>
+                            <DownloadRounded />
+                        </div>
                     </div>
                 }
                 closeIcon={<CloseRounded />}
@@ -56,12 +63,14 @@ const ImageCarouselDialog = ({
                 <Carousel
                     className="border-none rounded-xl mt-10 p-3 select-none"
                     opts={{ startIndex }}
+                    setApi={(api) => setCarouselApi(api)}
                 >
                     <CarouselContent className="h-full">
                         {files.map((value) => (
                             <CarouselItem
                                 key={value.id}
                                 className="flex justify-center"
+                                style={{ height: window.screen.height * 0.6 }}
                             >
                                 <img
                                     src={
@@ -69,7 +78,7 @@ const ImageCarouselDialog = ({
                                             ? URL.createObjectURL(value.file)
                                             : value.fileURL
                                     }
-                                    className="object-contain"
+                                    className={'object-scale-down'}
                                     style={{
                                         transform: `rotate(${rotation}deg)`,
                                         scale:
@@ -83,6 +92,40 @@ const ImageCarouselDialog = ({
                     </CarouselContent>
                     <CarouselPrevious className="left-0" />
                     <CarouselNext className="right-0" />
+                </Carousel>
+                <Carousel
+                    className="flex justify-center select-none"
+                    opts={{ skipSnaps: true }}
+                >
+                    <CarouselContent>
+                        {files.map((value, index) => (
+                            <CarouselItem
+                                key={value.id}
+                                className="basis-auto"
+                                onClick={() => carouselApi?.scrollTo(index)}
+                            >
+                                <div className="relative rounded-xl overflow-hidden">
+                                    <div
+                                        className={cn(
+                                            'absolute w-full h-full',
+                                            slidesInView[0] !== index &&
+                                                'bg-[#10141A] bg-opacity-60'
+                                        )}
+                                    />
+                                    <img
+                                        src={
+                                            value.file
+                                                ? URL.createObjectURL(
+                                                      value.file
+                                                  )
+                                                : value.fileURL
+                                        }
+                                        className="w-[150px] h-[100px] object-cover"
+                                    />
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
                 </Carousel>
             </DialogContent>
         </Dialog>
