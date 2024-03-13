0feb1fc44dd2b8c2b13fb976ff8093488839ac86
Ogannes Arustamian <toxicyouth@ya.ru>
2024-03-13 09:35:11 +0300

    fix bugs, add spinning refetch button, remove unused code and modules (#164)
    Co-authored-by: Ogannes Arustamian <toxicyouth@yandex.ru>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
-import { defineConfig } from 'vite'
-import svgr from "vite-plugin-svgr";
+import path from 'path'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react(), svgr({
-    include: '**/*.svg',
-  })],
-  build: {
-    target: 'esnext'
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        splitVendorChunkPlugin(),
+        svgr({
+            include: '**/*.svg',
+        }),
+    ],
+    build: {
+        target: 'esnext',
     },
-  },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
